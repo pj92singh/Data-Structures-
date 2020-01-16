08d4a1ac8c7cfce33bed9c6d6f19d65b925d0872
Prabhjit Singh <pj92singh@gmail.com>
2020-01-16 10:21:23 -0500

    Add files via upload

---
+++ ConsoleApp8/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using ConsoleApp8.Exceptions; 
+
+
+namespace ConsoleApp8
+{
+    /* pj92singh
+     *consoleApp 8 
+     *
+     *
+     **/
+    class Program
+    {
+        /* app 8 will use the interface IReporter 
+         * Report class and then the related exceptions classes
+         * to us the pattern concenpts of C# and show the correct logs
+         
+          the relationship is as follows; 
+          ****class and interface structure *****
+            // IReporter.cs interface // 
+            => use in Reporter.cs
+            both "use"/import the Execptions class 
+            >> Exceptions << 
+            -uses namespace of Exceptions 
+            DB // File // Http
+
+            Main ConsoleApp8 (using execptions)
+          */
+        static void Main(string[] args)
+        {
+            try {
+                //throw new DatabaseException("DB connection failed", "API DB");
+                throw new HttpException("HTTP error", "http//www.netsite.com", 503); 
+            }
+            catch (Exception err){
+                //testing with level error
+                IReporter reporter = new Reporter();
+                reporter.Report(err, "Something went wrong in the main method", SeverityLevel.Error);
+                reporter.Report(err, "Something went wrong in the main method", SeverityLevel.Info);
+                //we can contiune testing with other memebers of severitylevel and different exceptions
+
+            }
+            Console.ReadKey();
+        }
+    }
+}
