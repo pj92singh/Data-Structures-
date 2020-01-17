051a5930fea27bb862f43533b7ea42804684c6c0
Prabhjit Singh <pj92singh@gmail.com>
2020-01-17 10:01:12 -0500

    Add files via upload
    interface file that will be the base for the severitylevel

---
+++ ConsoleApp8/IReporter.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using ConsoleApp8.Exceptions; 
+
+namespace ConsoleApp8
+{
+    /* interface file 
+     * this will use the execeptions class
+     * and it will be the base file for severity level
+     */
+    public enum SeverityLevel
+    {
+        Trace = 0,
+        Info = 1,
+        Warning = 2,
+        Error = 3,
+        Critical = 4
+    }
+    public interface IReporter
+    {
+        //all classes can use this method from this interface
+        //original method
+        //public void Report(Exception ex, string description, SeverityLevel level);
+        //PART 2 
+        //it was changed to add the exeception paramter after
+        //VS studio will now warn us => reproter class doesn't have this added yet
+        public void Report(Exception ex, string description, SeverityLevel level); 
+    }
+}
