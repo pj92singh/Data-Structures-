3d9480dec221d2aa771fe0b17fb2586b0488f016
Prabhjit Singh <pj92singh@gmail.com>
2020-01-24 11:26:04 -0500

    Add files via upload

---
+++ ConsoleApp8/Exceptions/FileSystemException.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp8.Exceptions
+{
+	/* 
+	*example exception class 2 for Filesystem and files
+	* uses the a public method for throwing file system errors
+	* will be used in the main program along with the reporter interface
+	*/
+    public class FileSystemException : Exception
+    {
+        public FileSystemException(string message) : base(message)
+        {
+        }
+    }
+}
