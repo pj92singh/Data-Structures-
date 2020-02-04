d2cc8682d5c6f9f036bc6cf12c1af93a68fa182b
Prabhjit Singh <pj92singh@gmail.com>
2020-02-04 12:24:37 -0500

    Add files via upload

---
+++ ConsoleApp8/Exceptions/HttpException.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp8.Exceptions
+{
+    /* HttpExeception class
+     * will use the related Reporter class */
+    public class HttpException : Exception
+    {
+        public string Url { get; }
+        public int StatusCode { get; }
+        public HttpException(string message, string url, int statusCode) : base(message)
+        {
+            Url = url;
+            StatusCode = statusCode;
+        }
+    }
+}
