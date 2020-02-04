65cf65374c83516a70e869adae4b5e2b5cab6be2
Prabhjit Singh <pj92singh@gmail.com>
2020-02-04 12:25:54 -0500

    Update HttpException.cs

--- ConsoleApp8/Exceptions/HttpException.cs
+++ ConsoleApp8/Exceptions/HttpException.cs
@@ -5,7 +5,11 @@ using System.Text;
 namespace ConsoleApp8.Exceptions
 {
     /* HttpExeception class
-     * will use the related Reporter class */
+     * will use the related Reporter class 
+     * example exception class 3 for http requests and errors
+	* uses the a public method for throwing http system errors
+	* will be used in the main program along with the reporter interface
+	*/*/
     public class HttpException : Exception
     {
         public string Url { get; }
